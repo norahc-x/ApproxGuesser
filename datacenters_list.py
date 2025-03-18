8c6b9658d1a21ea3bc7d9eb1e865e7a1314fcc06
norahc <Norahc968@gmail.com>
2025-03-18 20:38:28 +0100

    Create datacenters_list.py

---
+++ datacenters_list.py
@@ -0,0 +1,232 @@
+datacenters = [
+     {
+        "url": "https://cdn-newyork.discordapp.com",
+        "city": "New York",
+        "lat": 40.7128,
+        "lon": -74.0060
+    },
+    {
+        "url": "https://cdn-losangeles.discordapp.com",
+        "city": "Los Angeles",
+        "lat": 34.0522,
+        "lon": -118.2437
+    },
+    {
+        "url": "https://cdn-chicago.discordapp.com",
+        "city": "Chicago",
+        "lat": 41.8781,
+        "lon": -87.6298
+    },
+    {
+        "url": "https://cdn-houston.discordapp.com",
+        "city": "Houston",
+        "lat": 29.7604,
+        "lon": -95.3698
+    },
+    {
+        "url": "https://cdn-phoenix.discordapp.com",
+        "city": "Phoenix",
+        "lat": 33.4484,
+        "lon": -112.0740
+    },
+    {
+        "url": "https://cdn-philadelphia.discordapp.com",
+        "city": "Philadelphia",
+        "lat": 39.9526,
+        "lon": -75.1652
+    },
+    {
+        "url": "https://cdn-sanantonio.discordapp.com",
+        "city": "San Antonio",
+        "lat": 29.4241,
+        "lon": -98.4936
+    },
+    {
+        "url": "https://cdn-sandiego.discordapp.com",
+        "city": "San Diego",
+        "lat": 32.7157,
+        "lon": -117.1611
+    },
+    {
+        "url": "https://cdn-dallas.discordapp.com",
+        "city": "Dallas",
+        "lat": 32.7767,
+        "lon": -96.7970
+    },
+    {
+        "url": "https://cdn-sanfrancisco.discordapp.com",
+        "city": "San Francisco",
+        "lat": 37.7749,
+        "lon": -122.4194
+    },
+    {
+        "url": "https://cdn-austin.discordapp.com",
+        "city": "Austin",
+        "lat": 30.2672,
+        "lon": -97.7431
+    },
+    {
+        "url": "https://cdn-jacksonville.discordapp.com",
+        "city": "Jacksonville",
+        "lat": 30.3322,
+        "lon": -81.6557
+    },
+    {
+        "url": "https://cdn-fortworth.discordapp.com",
+        "city": "Fort Worth",
+        "lat": 32.7555,
+        "lon": -97.3308
+    },
+    {
+        "url": "https://cdn-columbus.discordapp.com",
+        "city": "Columbus",
+        "lat": 39.9612,
+        "lon": -82.9988
+    },
+    {
+        "url": "https://cdn-indianapolis.discordapp.com",
+        "city": "Indianapolis",
+        "lat": 39.7684,
+        "lon": -86.1581
+    },
+    {
+        "url": "https://cdn-charlotte.discordapp.com",
+        "city": "Charlotte",
+        "lat": 35.2271,
+        "lon": -80.8431
+    },
+    {
+        "url": "https://cdn-seattle.discordapp.com",
+        "city": "Seattle",
+        "lat": 47.6062,
+        "lon": -122.3321
+    },
+    {
+        "url": "https://cdn-denver.discordapp.com",
+        "city": "Denver",
+        "lat": 39.7392,
+        "lon": -104.9903
+    },
+    {
+        "url": "https://cdn-washingtondiscordapp.com",
+        "city": "Washington, DC",
+        "lat": 38.9072,
+        "lon": -77.0369
+    },
+    # Europa
+    {
+        "url": "https://cdn-london.discordapp.com",
+        "city": "London",
+        "lat": 51.5074,
+        "lon": -0.1278
+    },
+    {
+        "url": "https://cdn-paris.discordapp.com",
+        "city": "Paris",
+        "lat": 48.8566,
+        "lon": 2.3522
+    },
+    {
+        "url": "https://cdn-berlin.discordapp.com",
+        "city": "Berlin",
+        "lat": 52.5200,
+        "lon": 13.4050
+    },
+    {
+        "url": "https://cdn-madrid.discordapp.com",
+        "city": "Madrid",
+        "lat": 40.4168,
+        "lon": -3.7038
+    },
+    {
+        "url": "https://cdn-rome.discordapp.com",
+        "city": "Rome",
+        "lat": 41.9028,
+        "lon": 12.4964
+    },
+    {
+        "url": "https://cdn.discordapp.com",
+        "city": "Milano",
+        "lat": 45.4642,
+        "lon": 9.1900
+    },
+    {
+        "url": "https://cdn1.discordapp.com",
+        "city": "Torino",
+        "lat": 45.0703,
+        "lon": 7.6869
+    },
+    {
+        "url": "https://cdn3.discordapp.com",
+        "city": "Palermo",
+        "lat": 38.1157,
+        "lon": 13.3615
+    },
+    {
+        "url": "https://cdn4.discordapp.com",
+        "city": "Bari",
+        "lat": 41.1171,
+        "lon": 16.8719
+    },
+    {
+        "url": "https://cdn-amsterdam.discordapp.com",
+        "city": "Amsterdam",
+        "lat": 52.3676,
+        "lon": 4.9041
+    },
+    {
+        "url": "https://cdn-brussels.discordapp.com",
+        "city": "Brussels",
+        "lat": 50.8503,
+        "lon": 4.3517
+    },
+    {
+        "url": "https://cdn-vienna.discordapp.com",
+        "city": "Vienna",
+        "lat": 48.2082,
+        "lon": 16.3738
+    },
+    {
+        "url": "https://cdn-zurich.discordapp.com",
+        "city": "Zurich",
+        "lat": 47.3769,
+        "lon": 8.5417
+    },
+    {
+        "url": "https://cdn-stockholm.discordapp.com",
+        "city": "Stockholm",
+        "lat": 59.3293,
+        "lon": 18.0686
+    },
+    {
+        "url": "https://cdn-oslo.discordapp.com",
+        "city": "Oslo",
+        "lat": 59.9139,
+        "lon": 10.7522
+    },
+    {
+        "url": "https://cdn-lisbon.discordapp.com",
+        "city": "Lisbon",
+        "lat": 38.7223,
+        "lon": -9.1393
+    },
+    {
+        "url": "https://cdn-dublin.discordapp.com",
+        "city": "Dublin",
+        "lat": 53.3498,
+        "lon": -6.2603
+    },
+    {
+        "url": "https://cdn-helsinki.discordapp.com",
+        "city": "Helsinki",
+        "lat": 60.1699,
+        "lon": 24.9384
+    },
+    {
+        "url": "https://cdn-prague.discordapp.com",
+        "city": "Prague",
+        "lat": 50.0755,
+        "lon": 14.4378
+    }
+
+]
